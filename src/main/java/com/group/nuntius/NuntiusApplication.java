1ec2bab30c41537d86f4f0c8bd85bb533f3a061f
Stefan Belk <belk.stefan@gmail.com>
2019-03-23 09:12:26 +0100

    Setup

--- src/main/java/com/group/nuntius/NuntiusApplication.java
+++ src/main/java/com/group/nuntius/NuntiusApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NuntiusApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.location", "target/application.properties");
 		SpringApplication.run(NuntiusApplication.class, args);
 	}
 
