fbbcce7fd0ed7ef2709a00910472aec957103932
paolof76 <ferrante.paolo@gmail.com>
2019-03-23 11:27:50 +0100

    Add Client rest controller

---
+++ src/main/java/com/group/nuntius/service/ClientRestController.java
@@ -0,0 +1,25 @@
+package com.group.nuntius.service;
+
+import com.group.nuntius.model.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/client")
+public class ClientRestController {
+
+    @Autowired
+    ClientRepository clientRepository;
+
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    public Client getById(@RequestParam Long id) {
+        return clientRepository.findById(id).get();
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public void create(Client client) {
+        clientRepository.save(client);
+    }
+
+
+}
