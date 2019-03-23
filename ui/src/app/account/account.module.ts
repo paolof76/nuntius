57130cd2e45f36829c0b08f833c4dbcb07d1e0f8
Yves Hendseth <yves.hendseth@hispeed.ch>
2019-03-23 16:29:20 +0100

    added dummy card

--- ui/src/app/account/account.module.ts
+++ ui/src/app/account/account.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {AccountRoutingModule} from "./account-routing.module";
 import {AccountComponent} from "./account.component";
 import {CommonModule} from "@angular/common";
+import {MzCardModule} from "ngx-materialize";
 
 @NgModule({
   imports: [
     CommonModule,
     AccountRoutingModule,
+    MzCardModule
   ],
   declarations: [
     AccountComponent
